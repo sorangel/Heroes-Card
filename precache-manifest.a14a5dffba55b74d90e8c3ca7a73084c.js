26272715b7de00efbc15efb55b4c149308adca66
Sorangel Contreras <solanzola933@mail.com>
2020-11-20 17:23:05 -0600

    Updates

---
+++ precache-manifest.a14a5dffba55b74d90e8c3ca7a73084c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e9e795c70b2c73362c225d4c276c6f",
+    "url": "/Heroes-Card/index.html"
+  },
+  {
+    "revision": "c87b59b81cfd9ab90d68",
+    "url": "/Heroes-Card/static/js/2.04e0a884.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Heroes-Card/static/js/2.04e0a884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca19662901cef27f55ee",
+    "url": "/Heroes-Card/static/js/main.6dd77592.chunk.js"
+  },
+  {
+    "revision": "c2c8bb9d18f5195db013",
+    "url": "/Heroes-Card/static/js/runtime-main.444ce9cc.js"
+  }
+]);
